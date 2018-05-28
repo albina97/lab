a5c537087102b0ce088edf9f3c97a4472b953722
albina97 <39708272+albina97@users.noreply.github.com>
2018-05-28 22:00:18 +0300

    Add files via upload

---
+++ lab_3_do_while/LR3_do_while/task2.c
@@ -0,0 +1,17 @@
+#include <math.h>
+#include "Header.h"
+double a, f;
+
+double summ2(double eps)
+{
+	a = eps + 1.0;
+	f = 0.0;
+	int i = 0;
+	do
+	{
+		a = pow(-1, i)*(1 / (2 *(i + 1)));
+		f += a;
+		++i;
+	} while (fabs(a) > eps);
+	return(f);
+}
